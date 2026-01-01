017788100136ad84822d57a713156b558bb7824d
Jay Prajapati <jayp37679@gmail.com>
2026-01-01 23:13:58 +0530

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       className={`font-mont w-full border-t-2 border border-dark border-x-0 font-medium text-lg dark:text-light dark:border-light sm:text-base sm:mt-10`}
     >
       <Layout className="flex items-center justify-between py-8 lg:flex-col lg:py-6 lg:gap-2 ">
-        <span>{new Date.now().getFullYear()} &copy; All Rights Reserved</span>
+        <span>{new Date().getFullYear()} &copy; All Rights Reserved</span>
         <div className="flex items-center">
           Build with{" "}
           <span className="p-1 text-xl text-primary dark:text-secondary">
